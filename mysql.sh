de953fdd4faf55f1abbe8049d6e68b1dc13c249f
Prav-Akula <pravs.akula@outlook.com>
2025-09-30 17:51:09 -0400

    shell-common-mysql

---
+++ mysql.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+source ./common.sh
+check_root
+
+dnf install mysql-server -y &>>$LOG_FILE
+VALIDATE $? "Installing mysql server"
+
+systemctl enable mysqld &>>$LOG_FILE
+VALIDATE $? "Enabling mysqld"
+
+systemctl start mysqld &>>$LOG_FILE
+VALIDATE $? "Starting mysql server"
+
+mysql_secure_installation --set-root-pass RoboShop@1
+VALIDATE $? "Setting up root password"
+
+print_total_time
